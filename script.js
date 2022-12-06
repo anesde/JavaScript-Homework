1040b783f203a9865239bd64a1830310dea480a9
anesde <demisoski11@gmail.com>
2022-12-06 14:48:10 +0100

    JavaScript Homework

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("Connected")
+
+let constPrice = 119.95;
+let userInput = 30;
+let taxRate = 5;
+
+let priceOfAll = constPrice * userInput;
+console.log(priceOfAll);
+
+let sumWithTax = constPrice * (taxRate/100);
+console.log(sumWithTax);
+
+
+
