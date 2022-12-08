9a95e140353052a35558885e71d242ff09fa1dbc
anesde <117676669+anesde@users.noreply.github.com>
2022-12-08 01:42:01 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
-console.log("Connected")
-
-let constPrice = 119.95;
-let userInput = 30;
-let taxRate = 5;
-
-let priceOfAll = constPrice * userInput;
-console.log(priceOfAll);
-
-let sumWithTax = constPrice * (1+(taxRate/100));
-console.log(sumWithTax);
-
-let priceTotal = sumWithTax * userInput;
-console.log(priceTotal);
+let yearInput = prompt("Enter a year");
+console.log(yearInput);
+let parsedInput = parseInt(yearInput);
+console.log(typeof yearInput);
 
 
-
-
-
+if(!Number.isNaN(parsedInput)){
+    if((yearInput - 4) %12 === 0){
+        alert("Rat")
+    } else if((yearInput - 4) %12 === 1){
+        alert("Ox");
+    } else if((yearInput - 4) %12 === 2){
+        alert("Tiger");
+    } else if((yearInput - 4) %12 === 3){
+        alert("Rabbit");
+    } else if((yearInput - 4) %12 === 4){
+        alert("Dragon");
+    } else if((yearInput - 4) %12 === 5){
+        alert("Snake");
+    } else if((yearInput - 4) %12 === 6){
+        alert("Horse");
+    } else if((yearInput - 4) %12 === 7){
+        alert("Goat");
+    } else if((yearInput - 4) %12 === 8){
+        alert("Monkey");
+    } else if((yearInput - 4) %12 === 9){
+        alert("Rooster");
+    } else if((yearInput - 4) %12 === 10){
+        alert("Dog");
+    } else if((yearInput - 4) %12 === 11){
+        alert("Pig");
+    }
+} else {
+    console.log("Not valid input");
+}
 
