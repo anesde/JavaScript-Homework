253b98eb973d6ddfd38c4a3feac2a44e98c6cae9
anesde <117676669+anesde@users.noreply.github.com>
2022-12-13 15:52:26 +0100

    Add files via upload

---
+++ JavaScript HomeWork1/JavaScript-Homework/script.js
@@ -0,0 +1,17 @@
+console.log("Connected")
+
+let constPrice = 119.95;
+let userInput = 30;
+let taxRate = 5;
+
+let priceOfAll = constPrice * userInput;
+console.log(priceOfAll);
+
+let sumWithTax = constPrice * (1+(taxRate/100));
+console.log(sumWithTax);
+
+let priceTotal = sumWithTax * userInput;
+console.log(priceTotal);
+
+
+
