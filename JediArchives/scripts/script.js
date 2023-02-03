538aeb8632bc9caf2674f003bf9be41cbef912cf
anesde <117676669+anesde@users.noreply.github.com>
2023-02-03 21:09:10 +0100

    Add files via upload

--- JediArchives/scripts/script.js
+++ JediArchives/scripts/script.js
@@ -3,14 +3,16 @@ let navigationService = {
     shipBtn: document.getElementById('btnShip'),
     nextBtn: document.getElementById('next'),
     prevBtn: document.getElementById('prev'),
+    table: document.getElementById('divTable'),
     currentPage: 1,
     btnPressed: '',
+    sortBy: null,
+    isAsc: true,
     registerEventListener: function () {
         this.peopleBtn.addEventListener('click', function () {
             // console.log('abc2');
             navigationService.btnPressed = 'people';
             apiService.getPeople(navigationService.currentPage);
-
         })
         this.shipBtn.addEventListener('click', function () {
             // console.log('abc3');
@@ -38,22 +40,69 @@ let navigationService = {
             }
             // console.log(navigationService.currentPage)
         })
-    }
+        this.table.addEventListener('click', (event) => {
+            let array = ['Name', 'Height', 'Mass', 'Gender', 'Birth Year', 'Appearences'];
+            let maps = [
+                {
+                    header: "Birth Year",
+                    property: 'birth_year'
+                },
+                {
+                    header: "Name",
+                    property: 'name'
+                },
+                {
+                    header: "Height",
+                    property: 'height'
+                },
+                {
+                    header: "Mass",
+                    property: 'mass'
+                }, {
+                    header: "Gender",
+                    property: 'gender'
+                },
+                {
+                    header: "Appearences",
+                    property: 'films'
+                }
+            ]
+            if (event.target.nodeName === 'TH' && array.includes(event.target.id)) {
+                let mapedProp = maps.find(x => x.header === event.target.id);
+                if (navigationService.sortBy === null) {
+                    navigationService.sortBy = mapedProp.property;
+                    navigationService.isAsc = true;
+                } else {
+                    navigationService.isAsc = !navigationService.isAsc;
+                }
+                apiService.getPeople(navigationService.currentPage, navigationService.sortBy, navigationService.isAsc)
+            }
+            // console.log(event.target.nodeName);
+        })
+    },
+
 }
 
 let apiService = {
     baseUrl: "https://swapi.dev/api/",
-    getPeople: function (page) {
+    getPeople: function (page, sortBy = null, isAsc = true) {
         let peopleUrl = `${this.baseUrl}people/?page=${page}`;
         fetch(peopleUrl)
             .then(function (response) {
                 return response.json();
             }).then(function (data) {
+                if (sortBy) {
+                    data = {
+                        ...data,
+                        results: isAsc ? data.results.sort((x, y) => x[sortBy].localeCompare(y[sortBy])) : data.results.sort((x, y) => y[sortBy].localeCompare(x[sortBy]))
+                    }
+                }
                 // check for next page
+                uiService.toggleButtons(data.next, data.previous)
                 // loader
                 // show result
-                uiService.showPeople(data.results)
-                console.log(data);
+                uiService.createTable(data.results);
+                // uiService.showShips(data.results)
             }).catch(function (error) {
                 console.log(error);
             })
@@ -64,60 +113,80 @@ let apiService = {
             .then(function (response) {
                 return response.json();
             }).then(function (data) {
+                uiService.toggleButtons(data.next, data.previous);
                 uiService.showShips(data.results);
             })
-    }
+    },
 }
 
 // apiService.getPeople(3);
 let uiService = {
-    showPeople: function (people) {
-        let table = document.getElementById('table');
-        let html = "";
-        for (let person of people) {
-            html += `<tr><th>Name</th>
-            <th>Height</th>
-            <th>Mass</th>
-            <th>Gender</th>
-            <th>Birth Year</th>
-            <th>Appearences</th>
-            <tr><td>${person.name}</td>
-            <td>${person.height}</td>
-            <td>${person.mass}</td>
-            <td>${person.gender}</td>
-            <td>${person.birth_year}</td>
-            <td>${person.films.length}</td></tr></tr>`
+    createTableHeader: function (table) {
+        let array = ['Name', 'Height', 'Mass', 'Gender', 'Birth Year', 'Appearences'];
+        let tr = document.createElement('tr');
+        for (let item of array) {
+            let th = document.createElement('th');
+            th.id = item;
+            tr.appendChild(th).innerText = item;
         }
-        table.innerHTML = html;
+        table.appendChild(tr);
     },
-    showShips: function (ships) {
-        let table = document.getElementById('table');
-        let html = "";
-        for (let ship of ships) {
-            html += `<tr><th>Name</th>
-            <th>Mode;</th>
-            <th>Manufaturer</th>
-            <th>Cost</th>
-            <th>People Capacity</th>
-            <th>Class</th>
-            <tr><td>${ship.name}</td>
-            <td>${ship.model}</td>
-            <td>${ship.manufacturer}</td>
-            <td>${ship.cost_in_credits}</td>
-            <td>${ship.passengers}</td>
-            <td>${ship.starship_class}</td></tr></tr>`
+
+    createTableBody: function (people, table) {
+        for (let person of people) {
+            let tr = document.createElement('tr');
+
+            let nameCol = document.createElement('td');
+            tr.appendChild(nameCol).innerText = person.name;
+
+            let heightCol = document.createElement('td');
+            tr.appendChild(heightCol).innerText = person.height;
+
+            let massCol = document.createElement('td');
+            tr.appendChild(massCol).innerText = person.mass;
+
+            let genderCol = document.createElement('td');
+            tr.appendChild(genderCol).innerText = person.gender;
+
+            let birth_yearCol = document.createElement('td');
+            tr.appendChild(birth_yearCol).innerText = person.birth_year;
+
+            let filmsCol = document.createElement('td');
+            tr.appendChild(filmsCol).innerText = person.films.length;
+
+            table.appendChild(tr);
         }
-        table.innerHTML = html;
+    },
+    createTable: function (data) {
+        let div = document.getElementById('divTable');
+        let table = document.createElement('table');
+        div.innerHTML = '';
+        table.style.margin = '0 auto';
+        table.setAttribute('border', '2');
+        div.appendChild(table);
+
+        uiService.createTableHeader(table);
+        uiService.createTableBody(data, table);
     },
     checkForPage: function (page) {
         return page === null;
     },
-    toggleBtn: function (toggle, btn) {
-        if (toggle) {
-
+    toggleButtons: function (toggleNext, togglePrev) {
+        if (toggleNext) {
+            navigationService.nextBtn.style.display = '';
+        } else {
+            navigationService.nextBtn.style.display = 'none';
         }
-    }
+        if (togglePrev) {
+            navigationService.prevBtn.style.display = '';
+        } else {
+            navigationService.prevBtn.style.display = 'none';
+        }
+    },
 
 }
 
 navigationService.registerEventListener();
+
+
+
